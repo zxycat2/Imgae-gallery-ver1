92825d9f715fc3e327c5d2a66c8b9082dfa0a2ee
周熙岩 <zxycat2@zhouxiyandemacbook-pro.local>
2018-11-22 18:41:02 +0000

    Created some basic stuff.

---
+++ Imgae gallery ver1/Collection_Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Collection_Cell.swift
+//  Imgae gallery ver1
+//
+//  Created by 周熙岩 on 2018/11/22.
+//  Copyright © 2018 DoDo. All rights reserved.
+//
+
+import UIKit
+
+class Collection_Cell: UICollectionViewCell {
+    
+}
---
+++ Imgae gallery ver1/Collection_ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  Collection_ViewController.swift
+//  Imgae gallery ver1
+//
+//  Created by 周熙岩 on 2018/11/22.
+//  Copyright © 2018 DoDo. All rights reserved.
+//
+
+import UIKit
+
+class Collection_ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collection: UICollectionView!{
+        didSet{
+            collection.dataSource = self
+            collection.delegate = self
+        }
+    }
+    
+    //有多少个cell
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    //每个cell怎么搞
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
