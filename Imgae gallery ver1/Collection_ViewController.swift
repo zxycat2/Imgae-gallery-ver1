95a3e9e0b0bab7c2e2215930588fd43c4ef9a76a
周熙岩 <zxycat2@zhouxiyandembp.staff.uws.loc>
2018-11-24 03:30:01 +0000

    Added the local drag and drop in the collectionView

--- Imgae gallery ver1/Collection_ViewController.swift
+++ Imgae gallery ver1/Collection_ViewController.swift
@@ -8,22 +8,81 @@
 
 import UIKit
 
-class Collection_ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class Collection_ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate{
+    //执行drop的函数
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
+        for item in coordinator.items{
+            //当drop来自内部
+            if let itemSourceIndexPath = item.sourceIndexPath{
+                if let imageName = item.dragItem.localObject{
+                    collectionView.performBatchUpdates({
+                        self.pciDic.remove(at: itemSourceIndexPath.item)
+                        self.pciDic.insert(imageName as! String, at: destinationIndexPath.item)
+                        self.collection.deleteItems(at: [itemSourceIndexPath])
+                        self.collection.insertItems(at: [destinationIndexPath])
+                    })
+                    //动画
+                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+                }
+            }
+        }
+        
+    }
+    //关于是否接受drop(目前是b内部拖拽就只需要uiimage，外部就同时需要image和url)
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        let judge = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        return judge ? session.canLoadObjects(ofClass: UIImage.self) : session.canLoadObjects(ofClass: UIImage.self)&&session.canLoadObjects(ofClass: NSURL.self)
+    }
+    //关于drop类型
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        let judge = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        return UICollectionViewDropProposal(operation: judge ? .move : .copy, intent: .insertAtDestinationIndexPath)
+        
+    }
+    
+    //dragd delegated要求的函数之一：提供beginning的drag item
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = collectionView
+        return makeDragItems(indexpath: indexPath)
+    }
+    
+    //提供后续添加的drag item
+    func collectionView(_ collectionView: UICollectionView, itmsForAdiingsession: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return makeDragItems(indexpath: indexPath)
+    }
+    //查找cell并转化为drag item，来提供给delegate
+    //todo:待修改，也许需要同时给一个url
+    func makeDragItems(indexpath: IndexPath) -> [UIDragItem]{
+        if let image = (self.collection.cellForItem(at: indexpath) as? Collection_Cell)?.viewInCollectionCell.backgroundImage{
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: image))
+            //local data
+            dragItem.localObject = self.pciDic[indexpath.item]
+            return [dragItem]
+        }else{
+            return []
+        }
+    }
+    
     
 
     @IBOutlet weak var collection: UICollectionView!{
         didSet{
             collection.dataSource = self
             collection.delegate = self
+            collection.dragDelegate = self
+            collection.dropDelegate = self
         }
     }
     //临时
-    let pciDic = [ "twogirls", "Megumi", "onion"]
+    var pciDic = [ "twogirls", "Megumi", "onion"]
     //有多少个cell
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
     //cell高度
+    //cell的显示倍数
+    var scale = CGFloat(0.5)
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         print("size"+String(indexPath.item))
@@ -37,7 +96,7 @@ class Collection_ViewController: UIViewController, UICollectionViewDataSource, U
         print(floatImageWidth)
         let aspectRatio = floatImageHeight/floatImageWidth
         
-        return CGSize(width: 0.9*width, height: 0.9*CGFloat(Float(width)*aspectRatio))
+        return CGSize(width: self.scale*width, height: self.scale*CGFloat(Float(width)*aspectRatio))
         
     }
     
